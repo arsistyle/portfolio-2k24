9e65c173d21fc203ed9dc82c67931da93defb4ec
Israel Larrondo <arsistyle@gmail.com>
2023-09-25 09:03:16 -0300

    [FIX] locale i18n nav

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -12,8 +12,8 @@ export default function Header({locale}: {locale: string}) {
         <div className='flex items-center gap-8 ml-auto'>
           <Nav />
           <div className="flex rounded-xl text-dark dark:text-white overflow-hidden bg-dark/10 dark:bg-white/10">
-            <Link href="/en" className={`px-1 pl-2 transition ${locale === 'en' ? 'opacity-100 bg-dark/10 dark:bg-white/10': 'opacity-60'}`}>EN</Link>
-            <Link href="/es" className={`px-1 pr-2 transition ${locale === 'es' ? 'opacity-100 bg-dark/10 dark:bg-white/10': 'opacity-60'}`}>ES</Link>
+            <Link href="/en" locale="en" className={`px-1 pl-2 transition ${locale === 'en' ? 'opacity-100 bg-dark/10 dark:bg-white/10': 'opacity-60'}`}>EN</Link>
+            <Link href="/es" locale="es" className={`px-1 pr-2 transition ${locale === 'es' ? 'opacity-100 bg-dark/10 dark:bg-white/10': 'opacity-60'}`}>ES</Link>
           </div>
         </div>
       </div>
